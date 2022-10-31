9af1857b51ed118dc23370b2c56e7e7d31058b42
khgxsss <doodoo1113@gmail.com>
2022-10-31 16:37:42 +0900

    5

---
+++ merge.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int number = 8;
+
+int size;
+int sorted[8]; // 정렬 배열 : 전역변수
+int count = 0;
+
+void merge(int a[], int m, int middle, int n) {
+    int i = m;
+    int j = middle +1;
+    int k = m;
+
+    while (i <=middle && j <= n)
+    {
+        if (a[i] <=a[j])
+        {
+            /* code */
+        }
+        
+    }
+    
+}
